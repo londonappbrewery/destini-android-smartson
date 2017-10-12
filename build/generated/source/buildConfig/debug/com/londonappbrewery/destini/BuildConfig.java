68f14f7ee70558065cda342457c8269f530a23bb
Roger Mak <roger.mak@rogers.com>
2017-10-11 23:29:08 -0400

    Add files via upload

---
+++ build/generated/source/buildConfig/debug/com/londonappbrewery/destini/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.londonappbrewery.destini;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.londonappbrewery.destini";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
